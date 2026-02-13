2218abba81912d0b8dd6e8f497417efe64dc0e8d
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-13 19:08:31 -0300

    feat: mobile menu

---
+++ app/components/Drawer.tsx
@@ -0,0 +1,57 @@
+import { useState, type ReactElement } from "react";
+import { Link } from "react-router";
+
+
+interface DrawerProps {
+  options: { label: string, link: string, icon: ReactElement }[]
+}
+
+
+export function Drawer(props: DrawerProps) {
+  const [isOpen, setIsOpen] = useState(true);
+  const { options } = props;
+
+  return (
+    <>
+      <button
+        role="button"
+        aria-label="menu-toggler"
+        className="order-first"
+        onClick={() => setIsOpen(true)}
+      >
+        <i className="fa-solid fa-bars fa-xl"></i>
+      </button>
+
+      {isOpen && (
+        <div
+          className="bg-black/10 fixed inset-0 z-0"
+          onClick={() => setIsOpen(false)}
+        >
+        </div>
+      )}
+
+      <nav>
+        <ul
+          className={` 
+          fixed top-0 left-0 z-100 
+          bg-white text-slate-800 
+          flex flex-col gap-6 p-4 
+          w-[80%] max-w-75
+          h-screen shadow-lg
+          transition-all duration-300
+          ${!isOpen && "-translate-x-full"}
+          `}>
+
+          {options.map(({ label, link, icon }) => (
+            <Link
+              to={link}
+              className="hover:text-blue-400 flex items-center gap-4"
+              onClick={() => setIsOpen(false)}
+            >{icon}{label}</Link>
+          ))}
+
+        </ul>
+      </nav>
+    </>
+  );
+}
--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router";
+import { Drawer } from "./Drawer";
+
 export function TopBar() {
+  const options = [
+    { icon: <i className="fa-solid fa-house"></i>, label: "Home", link: "/" },
+    { icon: <i className="fa-solid fa-cart-shopping"></i>, label: "Carrinho", link: "/" },
+  ];
   return (
-    <header className="bg-blue-400 block w-full text-white py-4">
-      <h1 className="font-bold text-2xl">SHOPTRIX</h1>
+    <header className="flex items-center bg-blue-400 w-full gap-4 text-white py-4 responsible">
+      <h1 className="font-bold">SHOPTRIX</h1>
+      <Drawer options={options} />
     </header>
   );
 }
