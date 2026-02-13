637bca72a15e9cf5462c6a270e9073fa13930f5c
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-13 19:14:04 -0300

    feat: add cart route page

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -3,11 +3,11 @@ import { Drawer } from "./Drawer";
 export function TopBar() {
   const options = [
     { icon: <i className="fa-solid fa-house"></i>, label: "Home", link: "/" },
-    { icon: <i className="fa-solid fa-cart-shopping"></i>, label: "Carrinho", link: "/" },
+    { icon: <i className="fa-solid fa-cart-shopping"></i>, label: "Carrinho", link: "/carrinho" },
   ];
   return (
     <header className="flex items-center bg-blue-400 w-full gap-4 text-white py-4 responsible">
-      <h1 className="font-bold">SHOPTRIX</h1>
+      <h1 className="font-bold">SHOPTRIX <i className="fa-brands fa-opencart"></i></h1>
       <Drawer options={options} />
     </header>
   );
