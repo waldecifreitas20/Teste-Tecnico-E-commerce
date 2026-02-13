f51cdc79f47294d1f4de290505404e357fb1456d
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-13 17:55:54 -0300

    feat: top bar base

---
+++ app/components/TopBar.tsx
@@ -0,0 +1,7 @@
+export function TopBar() {
+  return (
+    <header className="bg-blue-400 block w-full text-white py-4">
+      <h1 className="font-bold text-2xl">SHOPTRIX</h1>
+    </header>
+  );
+}
