ab28178f0bc7e44b3cd7f1aaf5b2686518420e21
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-15 18:46:19 -0300

    style: products grid

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import type { Route } from "./+types/Home";
 import type { Product } from "~/types/product";
+import { ProductCard } from "~/components/ProductCard";
 
 export async function loader({ params }: Route.LoaderArgs) {
   try {
@@ -22,19 +23,9 @@ export default function Home({
     <main className="responsible">
       <h1>Produtos</h1>
 
-      <section className="grid grid-cols-2 gap-2">
+      <section className="grid grid-cols-2 sm:grid-cols-3 gap-2">
         {products.map(product => {
-          return <article className="bg-white border border-slate-200 rounded-md">
-            <div className="h-32 w-full overflow-clip">
-              <img className="w-full block" src={product.images[0]} alt={product.title} />
-            </div>
-            <div className="px-2 flex flex-col justify-between">
-              <p className="text-sm">{product.title}</p>
-              <p className="text-slate-800 font-semibold text-lg mb-4">R$ {product.price.toFixed(2)}</p>
-
-              <button className="cursor-pointer hover:bg-green-600 bg-green-500 text-white font-medium text-sm w-full py-2 rounded-md">Comprar</button>
-            </div>
-          </article>
+          return <ProductCard product={product} />
         })}
       </section>
 
