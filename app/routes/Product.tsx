748c1def4679848e6230b3218ab84147c9923b63
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-16 03:44:34 -0300

    feat: product page structure

--- app/routes/Product.tsx
+++ app/routes/Product.tsx
@@ -2,6 +2,7 @@ import { Box } from "~/components/Box";
 import { productsService } from "~/services/ProductService";
 import type { Route } from "../+types/root";
 import type { Product } from "~/types/product";
+import { Link } from "react-router";
 
 
 export async function loader({ params }: { params: { slug: string } }) {
@@ -21,20 +22,59 @@ export default function Product({ loaderData }: Route.ComponentProps) {
 
   return (
     <Box className="p-0 w-full">
+      <Link
+        to="../"
+        className="
+        py-2 px-4 m-2 
+        inline-block rounded-full
+        text-xs w-fit text-slate-700 bg-white border-slate-200 border
+        ">
+        <i className="fa-solid fa-arrow-left mr-1"></i>
+        Página Inicial
+      </Link>
       <section>
         <img src={product.images[0]} alt={product.title} />
       </section>
 
       <aside className="lg:w-2/3 border border-slate-200 p-4 rounded-md bg-white">
-        <p>{product.category.name}</p>
-        <h1 className="font-bold text-2xl">{product.title}</h1>
-        <p className="old-price">R$ {(product.price * 1.4).toFixed(2)}</p>
-        <p className="price">R$ {product.price.toFixed(2)}</p>
+        <section className="pb-28">
+          <p className="text-slate-400 uppercase text-sm">{product.category.name}</p>
+          <h1 className="font-bold text-2xl">{product.title}</h1>
+          <p className="old-price">R$ {(product.price * 1.4).toFixed(2)}</p>
+          <p className="price">R$ {product.price.toFixed(2)}</p>
 
+          <div className="flex gap-2 items-center ">
+            <label htmlFor="quantity">Quantidade:</label>
+            <input
+              type="number"
+              min={1}
+              max={100}
+              defaultValue={1}
+              className="w-16 px-4 py-2 rounded-md border border-slate-200" />
+          </div>
 
-          <button className="accent h-fit border border-green-500">Comprar</button>
-          <button className=" bg-slate-300 mt-2 h-fit w-full ">Adicionar ao carrinho</button>
-
+          <p
+            className="
+            text-slate-500 text-sm 
+            mt-6 py-4 
+            border-t border-slate-200"
+          >
+            {product.description}
+          </p>
+        </section>
+        {/* fixed bottom on mobile */}
+        <section
+          className="
+          fixed bottom-0 left-0 right-0 
+          block py-4 px-4
+          bg-white 
+          border-t border-slate-200">
+          <button className="accent h-fit border border-green-500">Comprar $ </button>
+          <button className=" bg-slate-300 mt-2 h-fit w-full ">
+            Adicionar ao carrinho
+            <i className="fa-solid fa-cart-plus ml-2"></i>
+          </button>
+        </section>
 
       </aside>
     </Box>
