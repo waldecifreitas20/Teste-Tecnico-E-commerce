5073dd9021b55f7559771a2542731d048fab8e51
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-16 00:20:16 -0300

    refactor: hide box from screeen readers

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -2,7 +2,9 @@ import type { PropsWithChildren } from "react";
 
 export function Box({ children, className }: PropsWithChildren & { className?: string }) {
   return (
-    <section className={`px-4 md:w-[80%] md:max-w-[1000px] mx-auto ${className}`}>
+    <section
+      aria-hidden="true"
+      className={`px-4 md:w-[80%] md:max-w-[1000px] mx-auto ${className}`}>
       {children}
     </section>
   );
