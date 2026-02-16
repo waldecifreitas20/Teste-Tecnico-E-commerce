e723532fced80cd25903e30b331f79e5a685bfd5
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-15 23:51:24 -0300

    style: badge over product image

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,18 +3,20 @@ import type { Product } from "~/types/product";
 export function ProductCard({ product }: { product: Product }) {
   return (
     <article
-      className="bg-white border h-fit border-slate-200 rounded-md">
+      className="bg-white border h-fit border-slate-200 rounded-md relative ">
+      <span
+        className="badge absolute top-2 right-0">Oferta!</span>
       <img
         className="h-42 w-full object-cover block"
         src={product.images[0]}
         alt={product.title} />
 
-      <section className="p-2 flex h-40 grow flex-col justify-between">
+      <section className="p-2 flex grow flex-col justify-between">
         <div>
-          <p className="bg-orange-400 text-xs text-white px-4 leading-5 w-fit rounded-md animate-pulse">Oferta!</p>
+
           <p className="text-sm">{product.title}</p>
           <p className="text-slate-400 line-through text-sm mt-2">R$ {(product.price * 1.4).toFixed(2)}</p>
-          <p className="text-slate-800 font-semibold text-lg mb-2">R$ {product.price.toFixed(2)}</p>
+          <p className="text-slate-800 font-semibold text-lg mb-4">R$ {product.price.toFixed(2)}</p>
         </div>
 
         <button className="accent">Comprar</button>
