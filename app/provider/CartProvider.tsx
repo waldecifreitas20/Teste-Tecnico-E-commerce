d4312c3f4b985ae6526a3562468e207e1cabc4e1
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-16 06:50:57 -0300

    refactor: remove uneed code

--- app/provider/CartProvider.tsx
+++ app/provider/CartProvider.tsx
@@ -15,31 +15,7 @@ export const CartContext = createContext<CartContextType>({} as CartContextType)
 
 
 export function CartProvider({ children }: PropsWithChildren) {
-  const [cart, setCart] = useState<CartItem[]>([
-    {
-      id: "0",
-      product:
-      {
-        "id": 16,
-        "title": "Classic White Tee - Timeless Style and Comfort",
-        "slug": "classic-white-tee-timeless-style-and-comfort",
-        "price": 73,
-        "description": "Elevate your everyday wardrobe with our Classic White Tee. Crafted from premium soft cotton material, this versatile t-shirt combines comfort with durability, perfect for daily wear. Featuring a relaxed, unisex fit that flatters every body type, it's a staple piece for any casual ensemble. Easy to care for and machine washable, this white tee retains its shape and softness wash after wash. Pair it with your favorite jeans or layer it under a jacket for a smart look.",
-        "category": {
-          "id": 1,
-          "name": "Category1771226586504",
-          "slug": "category1771226586504",
-          "image": "http://i.pravatar.cc/300?img=10",
-        },
-        "images": [
-          "https://i.imgur.com/Y54Bt8J.jpeg",
-          "https://i.imgur.com/SZPDSgy.jpeg",
-          "https://i.imgur.com/sJv4Xx0.jpeg"
-        ]
-      },
-      quantity: 10
-    }
-  ]);
+  const [cart, setCart] = useState<CartItem[]>([]);
 
   function addItem(product: Product, quantity: number = 1) {
     const item = cart.find((item) => item.product.id === product.id);
@@ -57,10 +33,9 @@ export function CartProvider({ children }: PropsWithChildren) {
 
   function updateQuantity(itemId: string, quantity: number) {
     if (quantity < 1) {
-      // Optional: remove item if quantity is less than 1 
-      // or just return if we want to enforce min 1
       return;
     }
+
     setCart((prevCart) =>
       prevCart.map((item) =>
         item.id === itemId ? { ...item, quantity } : item
