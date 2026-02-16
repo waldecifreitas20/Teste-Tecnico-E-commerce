cf32f7f3d135eb36af15c8d4ff069233f8dddab0
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-16 00:37:02 -0300

    refactor: set main html in root

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <TopBar />
-        {children}
+        <main>{children}</main>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,7 +21,7 @@ export default function Home({
   const products = loaderData as any as Product[];
 
   return (
-    <main>
+    <>
       <Box>
         <h1>Produtos</h1>
 
@@ -31,7 +31,7 @@ export default function Home({
           })}
         </ProductView>
       </Box>
-    </main >
+    </>
   );
 }
 
