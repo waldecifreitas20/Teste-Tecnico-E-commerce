9b6d47dee10ffb481dbb93175037a4b569d3cb8b
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-17 19:18:29 -0300

    feat: product page responsible

--- app/routes/Product.tsx
+++ app/routes/Product.tsx
@@ -27,7 +27,7 @@ export default function Product({ loaderData }: Route.ComponentProps) {
   }
 
   return (
-    <Box className="p-0 w-full">
+    <Box className="p-0 w-full md:py-4">
       <Link
         to="../"
         className="
@@ -39,52 +39,66 @@ export default function Product({ loaderData }: Route.ComponentProps) {
         Página Inicial
       </Link>
 
-      <section>
-        <ProductImages images={product.images} />
-      </section>
+      <div className="md:flex md:gap-10 justify-between">
+        <section className="md:w-1/2">
+          <ProductImages images={product.images} />
+        </section>
 
-      <aside className="lg:w-2/3 border border-slate-200 p-4 rounded-md bg-white">
-        <section className="pb-28">
-          <p className="text-slate-400 uppercase text-sm">{product.category.name}</p>
-          <h1 className="font-bold text-2xl">{product.title}</h1>
-          <p className="old-price">R$ {(product.price * 1.4).toFixed(2)}</p>
-          <p className="price">R$ {product.price.toFixed(2)}</p>
+        <aside className="md:w-sm border border-slate-200 p-4 rounded-md bg-white">
 
-          <QuantityInput initialValue={1} onChangeValue={(qtd) => qtdRef.current = qtd} />
+          <section className="pb-28 md:pb-0">
+            <p className="text-slate-400 uppercase text-sm">{product.category.name}</p>
+            <h1 className="font-bold text-2xl">{product.title}</h1>
+            <p className="old-price">R$ {(product.price * 1.4).toFixed(2)}</p>
+            <p className="price">R$ {product.price.toFixed(2)}</p>
 
-          <p
-            className="
+            <QuantityInput initialValue={1} onChangeValue={(qtd) => qtdRef.current = qtd} />
+
+            <p
+              className="
             text-slate-500 text-sm 
             mt-6 py-4 
             border-t border-slate-200"
-          >
-            {product.description}
-          </p>
-        </section>
+            >
+              {product.description}
+            </p>
+          </section>
 
-        {/* fixed bottom on mobile */}
-        <section
-          className="
-          fixed bottom-0 left-0 right-0 
-          block py-4 px-4
-          bg-white 
-          border-t border-slate-200">
-          <Link
-            to={"/carrinho"}
-            onClick={() => cart.addItem(product, qtdRef.current)}
-          ><button className="accent h-fit border border-green-500">
-              Comprar $
+          {/* fixed bottom on mobile */}
+          <section
+            className="
+            fixed bottom-0 left-0 right-0 
+            block py-4 px-4
+            bg-white 
+            border-t border-slate-200
+
+            md:static
+            
+            md:p-0 md:m-0
+            md:border-none
+          ">
+
+            <Link
+              to={"/carrinho"}
+              className="block w-full"
+              onClick={() => cart.addItem(product, qtdRef.current)}
+            ><button
+              className="accent block h-fit border border-green-500 w-full md:py-3">
+                Comprar $
+              </button>
+            </Link>
+
+            <button
+              onClick={() => cart.addItem(product, qtdRef.current)}
+              className=" bg-slate-300 md:py-3 hover:bg-slate-400 mt-2 h-fit w-full  ">
+              Adicionar ao carrinho
+              <i className="fa-solid fa-cart-plus ml-2 md:m-0"></i>
             </button>
-          </Link>
-          <button
-            onClick={() => cart.addItem(product, qtdRef.current)}
-            className=" bg-slate-300 mt-2 h-fit w-full ">
-            Adicionar ao carrinho
-            <i className="fa-solid fa-cart-plus ml-2"></i>
-          </button>
-        </section>
+          </section>
 
-      </aside>
+        </aside>
+
+      </div>
     </Box>
   )
 }
