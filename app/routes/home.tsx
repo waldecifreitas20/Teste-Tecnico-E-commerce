66aee59a6a24a9d8d45b5abf600a9d1fd826f7dc
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-17 18:39:20 -0300

    feat: responsible hero

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,12 @@
 import type { Route } from "./+types/Home";
+import { useMediaQuery } from "~/hooks/useMediaQuery";
 import type { Product } from "~/types/product";
 import { ProductCard } from "~/components/ProductCard";
 import { ProductView } from "~/components/ProductView";
 import { Box } from "~/components/Box";
 import { productsService } from "~/services/ProductService";
 import heroMobile from "~/assets/hero-mobile.svg";
+import heroDesktop from "~/assets/hero-desk.jpg";
 import { Link } from "react-router";
 
 
@@ -20,18 +22,18 @@ export default function Home({
   loaderData
 }: Route.ComponentProps) {
   const products: Product[] = loaderData;
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
     <>
       <img
         onClick={() => window.location.reload()}
         className="w-full mb-4"
-        src={heroMobile}
+        src={isDesktop ? heroDesktop : heroMobile}
         alt="Hero"
       />
-
+    
       <Box>
-
         <ProductView>
           {products.map(product => {
             return <ProductCard key={product.slug} product={product} />
