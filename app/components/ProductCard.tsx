12f80321dbe1305be7f2f365b6be2926edf1bfd3
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-17 20:08:44 -0300

    refactor: fix height and hover effects

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -7,19 +7,29 @@ export function ProductCard({ product }: { product: Product }) {
   return (
     <Link
       to={`/produto/${product.slug}`}
-      className="bg-white border h-fit border-slate-200 rounded-md relative ">
+      className="
+      bg-white 
+      border h-fit border-slate-200 rounded-md 
+      relative
+      hover:border-orange-500
+      hover:shadow-md
+      hover:shadow-orange-500/20
+      transition-all
+      duration-200
+      ease-in-out
+      ">
       <span
         className="badge absolute top-2 right-0">Oferta!</span>
 
       <Image src={product.images[0]} alt={product.title} />
 
-      <section className="p-2 flex grow flex-col justify-between">
+      <section className="p-2 flex flex-col justify-between h-32">
+        <p className="text-sm hover:text-orange-500">{product.title}</p>
         <div>
-          <p className="text-sm">{product.title}</p>
           <p className="text-slate-400 line-through text-sm mt-2">R$ {(product.price * 1.4).toFixed(2)}</p>
           <p className="text-slate-800 font-semibold text-lg mb-4">R$ {product.price.toFixed(2)}</p>
         </div>
       </section>
-    </Link>
+    </Link >
   );
 }
